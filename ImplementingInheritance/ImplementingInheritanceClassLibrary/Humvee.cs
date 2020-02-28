704e38a2617a2481470e027b0c78fefe15250c91
CollinHandel <31261553+CollinHandel@users.noreply.github.com>
2020-02-28 12:46:56 -0500

    Adding

---
+++ ImplementingInheritance/ImplementingInheritanceClassLibrary/Humvee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementingInheritanceClassLibrary
+{
+    public class Humvee : Vehicles
+    {
+        public override string Start() => "Press button";
+
+        public override string Move() => "Presses gas peddle";
+    }
+}
---
+++ ImplementingInheritance/ImplementingInheritanceClassLibrary/Weapons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementingInheritanceClassLibrary
+{
+    public class Weapons
+    {
+        public virtual string Load() => "Default load weapon";
+
+        public virtual string Fire() => "Default fire weapon";
+    }
+}
