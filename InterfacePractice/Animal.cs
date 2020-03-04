eb6cfc67d34057a92cf825463608bd0a587e4bc0
CollinHandel <31261553+CollinHandel@users.noreply.github.com>
2020-03-04 13:32:14 -0500

    Adding More

---
+++ InterfacePractice/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePractice
+{
+    public abstract  class Animal
+    {
+        public string Habitat { get; set; }
+
+        public virtual string Breathe()
+        {
+            return "Abstract breathing";
+        }
+    }
+}
